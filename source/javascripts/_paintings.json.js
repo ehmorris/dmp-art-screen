1193ad4ca69da856a0e162973254440542b0f81b
Edwin Morris <edwin@rapgenius.com>
2014-10-17 19:30:26 -0400

    add info sources to all images

--- source/javascripts/_paintings.json.js
+++ source/javascripts/_paintings.json.js
@@ -5,7 +5,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.wikiart.org/en/frantisek-kupka/cathedral-1913'
   },
   {
     filename: 'Georges-Pierre_Seurat_-_Honfleur_un_soir_embouchure_de_la_Seine_-_Google_Art_Project.jpg',
@@ -13,7 +13,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.wikiart.org/en/georges-seurat/mouth-of-the-seine-at-honfleur-evening-1886'
   },
   {
     filename: 'Goddess_Bhadrakali_Worshipped_by_the_Gods-_from_a_tantric_Devi_series_-_Google_Art_Project.jpg',
@@ -21,7 +21,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/Wikipedia:Featured_picture_candidates/Bhadrakali'
   },
   {
     filename: 'Hans_Holbein_der_Jungere_-_Der_Kaufmann_Georg_Gisze_-_Google_Art_Project.jpg',
@@ -29,7 +29,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/Georg_Giese'
   },
   {
     filename: 'In_the_Conservatory_-_edited.jpg',
@@ -37,7 +37,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.wikiart.org/en/edouard-manet/the-conservatory-1879'
   },
   {
     filename: 'James_McNeill_Whistler_-_La_Princesse_du_pays_de_la_porcelaine_-_brighter.jpg',
@@ -45,7 +45,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.wikiart.org/en/james-mcneill-whistler/the-princess-from-the-land-of-porcelain-1865'
   },
   {
     filename: 'James_McNeill_Whistler_-_Nocturne-_Battersea_Bridge_-_Google_Art_Project.jpg',
@@ -53,7 +53,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.wikiart.org/en/james-mcneill-whistler/nocturne-battersea-bridge'
   },
   {
     filename: 'Joseph_Mallord_William_Turner_-_The_Sun_of_Venice_Going_to_Sea_-_Google_Art_Project.jpg',
@@ -61,7 +61,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.tate.org.uk/art/artworks/turner-the-sun-of-venice-going-to-sea-n00535'
   },
   {
     filename: 'Juan_Gris_-_La_bouteille_danis_-_Google_Art_Project.jpg',
@@ -69,7 +69,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.museoreinasofia.es/en/collection/artwork/bouteille-danis-bottle-anis'
   },
   {
     filename: 'Max_Dupain_-_Adelaide_-_Google_Art_Project.jpg',
@@ -77,7 +77,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://commons.wikimedia.org/wiki/File:Max_Dupain_-_Adelaide_-_Google_Art_Project.jpg'
   },
   {
     filename: 'Pieter_Bruegel_the_Elder-_The_Harvesters_-_Google_Art_Project.jpg',
@@ -85,7 +85,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://www.metmuseum.org/toah/works-of-art/19.164'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Nachtwacht_-_Google_Art_Project.jpg',
@@ -93,7 +93,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/The_Night_Watch'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Return_of_the_Prodigal_Son_-_Google_Art_Project.jpg',
@@ -101,7 +101,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/The_Return_of_the_Prodigal_Son_(Rembrandt)'
   },
   {
     filename: 'Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
@@ -109,7 +109,7 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/The_Starry_Night'
   },
   {
     filename: 'Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg',
@@ -117,6 +117,6 @@ var paintings_json = [
     artist: 'placeholder',
     year: 'placeholder',
     details: 'placeholder',
-    source: 'placeholder'
+    source: 'http://en.wikipedia.org/wiki/Bedroom_in_Arles'
   }
 ]
