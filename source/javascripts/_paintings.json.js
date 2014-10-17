cfade826bed6b5e9bbb26c7c7c1de2e982c25f72
Edwin Morris <edwin@rapgenius.com>
2014-10-17 19:12:03 -0400

    update placeholder values, animate pan faster
    increased speed also means a short viewing period after the pan

--- source/javascripts/_paintings.json.js
+++ source/javascripts/_paintings.json.js
@@ -4,104 +4,119 @@ var paintings_json = [
     title: 'placeholder',
     artist: 'placeholder',
     year: 'placeholder',
-    details: ''
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Georges-Pierre_Seurat_-_Honfleur_un_soir_embouchure_de_la_Seine_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Goddess_Bhadrakali_Worshipped_by_the_Gods-_from_a_tantric_Devi_series_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Hans_Holbein_der_Jungere_-_Der_Kaufmann_Georg_Gisze_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'In_the_Conservatory_-_edited.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'James_McNeill_Whistler_-_La_Princesse_du_pays_de_la_porcelaine_-_brighter.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'James_McNeill_Whistler_-_Nocturne-_Battersea_Bridge_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Joseph_Mallord_William_Turner_-_The_Sun_of_Venice_Going_to_Sea_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Juan_Gris_-_La_bouteille_danis_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Max_Dupain_-_Adelaide_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Pieter_Bruegel_the_Elder-_The_Harvesters_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Nachtwacht_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Return_of_the_Prodigal_Son_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   },
   {
     filename: 'Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg',
     title: 'placeholder',
     artist: 'placeholder',
-    year: 'placeh ',
-    details: ''
+    year: 'placeholder',
+    details: 'placeholder',
+    source: 'placeholder'
   }
 ]
