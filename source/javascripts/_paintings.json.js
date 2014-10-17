db460c604421f7f72e164473f872349804c5c7cb
Edwin Morris <edwin@rapgenius.com>
2014-10-17 19:44:27 -0400

    fill in all painting info

--- source/javascripts/_paintings.json.js
+++ source/javascripts/_paintings.json.js
@@ -1,122 +1,122 @@
 var paintings_json = [
   {
     filename: 'Frantisek_Kupka_-_Katedrala_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'Cathedral',
+    artist: 'Frantisek Kupka',
+    year: '1913',
+    details: 'Oil on canvas',
     source: 'http://www.wikiart.org/en/frantisek-kupka/cathedral-1913'
   },
   {
     filename: 'Georges-Pierre_Seurat_-_Honfleur_un_soir_embouchure_de_la_Seine_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'Mouth of the Seine at Honfleur, evening',
+    artist: 'Georges Seurat',
+    year: '1886',
+    details: 'Oil on canvas',
     source: 'http://www.wikiart.org/en/georges-seurat/mouth-of-the-seine-at-honfleur-evening-1886'
   },
   {
     filename: 'Goddess_Bhadrakali_Worshipped_by_the_Gods-_from_a_tantric_Devi_series_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
-    source: 'http://en.wikipedia.org/wiki/Wikipedia:Featured_picture_candidates/Bhadrakali'
+    title: 'The Hindu goddess Bhadrakali worshipped by the Hindu trinity of male gods',
+    artist: 'Unknown',
+    year: '1660',
+    details: 'Opaque watercolor, gold, silver and beetle-wing cases on paper',
+    source: 'http://commons.wikimedia.org/wiki/File:Goddess_Bhadrakali_Worshipped_by_the_Gods-_from_a_tantric_Devi_series_-_Google_Art_Project.jpg'
   },
   {
     filename: 'Hans_Holbein_der_Jungere_-_Der_Kaufmann_Georg_Gisze_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
-    source: 'http://en.wikipedia.org/wiki/Georg_Giese'
+    title: 'The Merchant Georg Gisze',
+    artist: 'Hans Holbein the Younger',
+    year: '1532',
+    details: 'Oil on oak',
+    source: 'http://commons.wikimedia.org/wiki/File:Holbein,_Hans_-_Georg_Gisze,_a_German_merchant_in_London.jpg'
   },
   {
     filename: 'In_the_Conservatory_-_edited.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'In The Conservatory',
+    artist: 'Edouard Manet',
+    year: '1879',
+    details: 'Oil on canvas',
     source: 'http://www.wikiart.org/en/edouard-manet/the-conservatory-1879'
   },
   {
     filename: 'James_McNeill_Whistler_-_La_Princesse_du_pays_de_la_porcelaine_-_brighter.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
-    source: 'http://www.wikiart.org/en/james-mcneill-whistler/the-princess-from-the-land-of-porcelain-1865'
+    title: 'The Princess from the Land of Porcelain',
+    artist: 'James Whistler',
+    year: '1865',
+    details: 'Oil on canvas',
+    source: 'http://en.wikipedia.org/wiki/The_Princess_from_the_Land_of_Porcelain'
   },
   {
     filename: 'James_McNeill_Whistler_-_Nocturne-_Battersea_Bridge_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
-    source: 'http://www.wikiart.org/en/james-mcneill-whistler/nocturne-battersea-bridge'
+    title: 'Nocturne: Battersea Bridge',
+    artist: 'James Whistler',
+    year: '1873',
+    details: 'Pastel on brown paper',
+    source: 'http://commons.wikimedia.org/wiki/File:James_McNeill_Whistler_-_Nocturne-_Battersea_Bridge_-_Google_Art_Project.jpg'
   },
   {
     filename: 'Joseph_Mallord_William_Turner_-_The_Sun_of_Venice_Going_to_Sea_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Sun of Venice Going to Sea',
+    artist: 'William Turner',
+    year: '1843',
+    details: 'Oil paint on canvas',
     source: 'http://www.tate.org.uk/art/artworks/turner-the-sun-of-venice-going-to-sea-n00535'
   },
   {
     filename: 'Juan_Gris_-_La_bouteille_danis_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Bottle of Anis',
+    artist: 'Juan Gris',
+    year: '1914',
+    details: 'Oil, collage and graphite on canvas',
     source: 'http://www.museoreinasofia.es/en/collection/artwork/bouteille-danis-bottle-anis'
   },
   {
     filename: 'Max_Dupain_-_Adelaide_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'Adelaide',
+    artist: 'Max Dupain',
+    year: '1946',
+    details: 'Gelatin-silver photograph',
     source: 'http://commons.wikimedia.org/wiki/File:Max_Dupain_-_Adelaide_-_Google_Art_Project.jpg'
   },
   {
     filename: 'Pieter_Bruegel_the_Elder-_The_Harvesters_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Harvesters',
+    artist: 'Pieter Bruegel the Elder',
+    year: '1565',
+    details: 'Oil on wood',
     source: 'http://www.metmuseum.org/toah/works-of-art/19.164'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Nachtwacht_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Night Watch',
+    artist: 'Rembrandt van Rijn',
+    year: '1642',
+    details: 'Oil on canvas',
     source: 'http://en.wikipedia.org/wiki/The_Night_Watch'
   },
   {
     filename: 'Rembrandt_Harmensz_van_Rijn_-_Return_of_the_Prodigal_Son_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Return of the Prodigal Son',
+    artist: 'Rembrandt van Rijn',
+    year: '1668',
+    details: 'Oil on canvas',
     source: 'http://en.wikipedia.org/wiki/The_Return_of_the_Prodigal_Son_(Rembrandt)'
   },
   {
     filename: 'Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'The Starry Night',
+    artist: 'Vincent van Gogh',
+    year: '1889',
+    details: 'Oil on canvas',
     source: 'http://en.wikipedia.org/wiki/The_Starry_Night'
   },
   {
     filename: 'Vincent_van_Gogh_-_De_slaapkamer_-_Google_Art_Project.jpg',
-    title: 'placeholder',
-    artist: 'placeholder',
-    year: 'placeholder',
-    details: 'placeholder',
+    title: 'Bedroom in Arles',
+    artist: 'Vincent van Gogh',
+    year: '1888',
+    details: 'Oil on canvas',
     source: 'http://en.wikipedia.org/wiki/Bedroom_in_Arles'
   }
 ]
